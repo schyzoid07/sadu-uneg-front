bcaa074a7e67aa474e8088415d4dfb2d52cf04d5
miguel <schizoid07@gmail.com>
2025-12-27 16:52:26 -0400

    añadida busqueda react'query y tipado de zod a componente eventos

--- src/app/eventos/page.tsx
+++ src/app/eventos/page.tsx
@@ -1,3 +1,6 @@
+'use client'
+import * as z from "zod/v4";
+import ky from "ky";
 import { Button } from "@/components/ui/button";
 import {
   Table,
@@ -8,52 +11,34 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { useQuery } from "@tanstack/react-query";
 import { HammerIcon, Trash2Icon } from "lucide-react";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Eventos() {
-  const events = [
-    {
-      id: 1,
-      name: "Kristan",
-    },
-    {
-      id: 2,
-      name: "Hesther",
-    },
-    {
-      id: 3,
-      name: "Courtnay",
-    },
-    {
-      id: 4,
-      name: "Hall",
-    },
-    {
-      id: 5,
-      name: "Brittni",
-    },
-    {
-      id: 6,
-      name: "Lurette",
-    },
-    {
-      id: 7,
-      name: "Ricki",
-    },
-    {
-      id: 8,
-      name: "Phedra",
-    },
-    {
-      id: 9,
-      name: "Maxim",
-    },
-    {
-      id: 10,
-      name: "Salomone",
-    },
-  ];
+ const {data, isLoading} = useQuery({
+    queryKey: ["events"],
+    queryFn: async () => {
+      const eventsSchema = z.array(
+        z.object({
+          ID: z.number(),
+          Nombre: z.string(),
+        })
+      );
 
+      const resSchema = z.object({
+        data: eventsSchema,
+        message: z.string(),
+      });
+
+      const res = await ky.get("http://localhost:8080/events/").json();
+
+      const parsed = resSchema.parse(res);
+
+      return parsed.data;
+  }});
+
+  
   return (
     <Table>
       <TableCaption>A list of your recent invoices.</TableCaption>
@@ -61,14 +46,25 @@ export default function Eventos() {
         <TableRow>
           <TableHead>Id</TableHead>
           <TableHead>Nombre</TableHead>
+
           <TableHead></TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
-        {events.map((data) => (
-          <TableRow key={data.id}>
-            <TableCell>{data.id}</TableCell>
-            <TableCell>{data.name}</TableCell>
+        {isLoading
+          ? Array.from({ length: 10 }).map((_, i) => (
+              <TableRow key={i}>
+                <TableCell colSpan={7}>
+                  <Skeleton className="w-full h-10"></Skeleton>
+                </TableCell>
+              </TableRow>
+            ))
+          : null}
+        {data ?
+         data.map((data) => (
+          <TableRow key={data.ID}>
+            <TableCell>{data.ID}</TableCell>
+            <TableCell>{data.Nombre}</TableCell>
             <TableCell>
               <Button
                 size="icon"
@@ -86,8 +82,11 @@ export default function Eventos() {
               </Button>
             </TableCell>
           </TableRow>
-        ))}
+        ))
+        : null}
       </TableBody>
     </Table>
+
+    //add buttons of "añadir and delete"
   );
 }
