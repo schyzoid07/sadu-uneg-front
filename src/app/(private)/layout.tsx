062e78a51394232155df149b29463cf4d90a1556
schyzoid07 <108144045+schyzoid07@users.noreply.github.com>
2026-03-15 00:16:26 -0400

    Revert "Revert "Auth implementado""

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -7,7 +7,6 @@ import { AppSidebar } from "@/components/app-sidebar";
 import "../globals.css";
 import { Separator } from "@radix-ui/react-separator";
 import HeaderBar from "@/components/HeaderBar";
-import { Providers } from "@/components/Providers";
 
 export default function PrivateLayout({
     children,
@@ -16,26 +15,24 @@ export default function PrivateLayout({
 }>) {
     return (
         <div>
-            <Providers>
-                <SidebarProvider>
-                    <AppSidebar />
-                    <SidebarInset>
-                        <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-                            <SidebarTrigger className="-ml-1" />
+            <SidebarProvider>
+                <AppSidebar />
+                <SidebarInset>
+                    <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+                        <SidebarTrigger className="-ml-1" />
 
-                            <Separator
-                                orientation="vertical"
-                                className="mr-2 data-[orientation=vertical]:h-4"
-                            />
-                        </header>
+                        <Separator
+                            orientation="vertical"
+                            className="mr-2 data-[orientation=vertical]:h-4"
+                        />
+                    </header>
 
-                        <div className="flex flex-1 flex-col gap-4 p-4">
-                            <HeaderBar />
-                            {children}
-                        </div>
-                    </SidebarInset>
-                </SidebarProvider>
-            </Providers>
+                    <div className="flex flex-1 flex-col gap-4 p-4">
+                        <HeaderBar />
+                        {children}
+                    </div>
+                </SidebarInset>
+            </SidebarProvider>
         </div>
     );
 }
--- src/schemas/login-form.ts
+++ src/schemas/login-form.ts
@@ -1,4 +1,4 @@
-import { z } from "zod"
+import * as z from "zod"
 
 export const formSchema = z.object({
     username: z.email(),
